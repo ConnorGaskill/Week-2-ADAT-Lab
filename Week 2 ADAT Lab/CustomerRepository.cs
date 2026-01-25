71b3b32d9acc1c2124099985ae605fba39890467
Connor Gaskill <gaskillconnor@gmail.com>
2026-01-25 12:13:20 -0800

    Add project files.

---
+++ Week 2 ADAT Lab/CustomerRepository.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2_ADAT_Lab
+{
+    public class CustomerRepository
+    {
+        private readonly string _connectionString;
+        public CustomerRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<Customer> GetAll()
+        {
+            List<Customer> results = new List<Customer>();
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand(
+                    @"SELECT CustomerId,
+                     FirstName,
+                     LastName,
+                     Email,
+                     IsActive
+                     FROM dbo.Customers;", conn))
+                {
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Customer c = new Customer
+                            {
+                                CustomerId = reader.GetInt32(0),
+                                FirstName = reader.GetString(1),
+                                LastName = reader.GetString(2),
+                                Email = reader.GetString(3),
+                                IsActive = reader.GetBoolean(4)
+                            };
+
+                            results.Add(c);
+                        }
+                    }
+                }
+            }
+
+            return results;
+        }
+
+        public Customer? GetById(int id)
+        {
+            Customer? result = null;
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand(
+                    @"SELECT CustomerId,
+                     FirstName,
+                     LastName,
+                     Email,
+                     IsActive
+                     FROM dbo.Customers
+                     WHERE CustomerId = @Id;", conn))
+                {
+                    cmd.Parameters.AddWithValue("@Id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            result = new Customer
+                            {
+                                CustomerId = reader.GetInt32(0),
+                                FirstName = reader.GetString(1),
+                                LastName = reader.GetString(2),
+                                Email = reader.GetString(3),
+                                IsActive = reader.GetBoolean(4)
+                            };
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Week 2 ADAT Lab/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Week_2_ADAT_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString =
+                "Server=(localDB)\\MSSQLLocalDB;Database=IT410Week2Demo;" +
+                "Trusted_Connection=True;TrustServerCertificate=True;";
+
+            ADOTests(connectionString);
+        }
+
+        static void ADOTests(string connectionString)
+        {
+            CustomerRepository repo = new CustomerRepository(connectionString);
+
+            Console.WriteLine("[Testing Get All]\n");
+
+            foreach (Customer c in repo.GetAll())
+                Console.WriteLine($"{c.FirstName} {c.LastName}");
+
+            Console.WriteLine("\n[Testing getting valid ID (1)]\n");
+
+
+
+            Customer? customer = repo.GetById(1);
+
+            if (customer == null )
+            {
+                Console.WriteLine("Error: Valid customer ID returns null value");
+            }
+            else
+            {
+                Console.WriteLine($"Customer found: ID: {customer.CustomerId} {customer.FirstName} {customer.LastName}");
+            }
+
+            Console.WriteLine("\n[Testing getting invalid ID (-1)]\n");
+
+            customer = repo.GetById(-1);
+
+            if (customer == null)
+            {
+                Console.WriteLine("Invalid customer ID returns null value");
+            }
+            else
+            {
+                Console.WriteLine($" Error: Customer found: ID: {customer.CustomerId} {customer.FirstName} {customer.LastName}");
+            }
+        }
+    }
+}
